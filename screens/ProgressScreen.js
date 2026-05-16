b09dae18553a693e008a8e270f063bde6c715ea5
irensula <irinawebdev@gmail.com>
2026-05-16 20:29:29 +0300

    show progress

--- screens/ProgressScreen.js
+++ screens/ProgressScreen.js
@@ -10,7 +10,6 @@ const ProgressScreen = ({ navigation }) => {
   const API_BASE = Constants.expoConfig?.extra?.API_BASE;
   const { user, token } = useContext(AuthContext);
   const [userProgress, setUserProgress] = useState([]);
-  const [totalMaxScore, setTotalMaxScore] = useState(0);
 
   useEffect(() => {
     const fetchProgress = async () => {
@@ -24,56 +23,49 @@ const ProgressScreen = ({ navigation }) => {
       } catch (error) {
         console.error("Error fetching user progress:", error);
       }
-    };
+    };   
+
     fetchProgress();
+
   }, [user, token]);
 
-  useEffect(() => {
-    const fetchTotalMaxScore = async () => {
-      try {
-        if (!token || !user?.id) return;
+  const currentScoreAll = userProgress?.currentScoreAll || 0;
+  const maxScoreAll = userProgress?.maxScoreAll || 0;
 
-        const res = await fetch(`${API_BASE}/max-score`, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
+  const currentScoreEn = userProgress?.currentScoreEn || 0;
+  const currentScoreFi = userProgress?.currentScoreFi || 0;
+  const currentScoreUa = userProgress?.currentScoreUa || 0;
+  const currentScoreRu = userProgress?.currentScoreRu || 0;
 
-        const data = await res.json();
+  const maxScorePerLanguage = userProgress?.maxScorePerLanguage || 0;      
+    const percentAll =
+    maxScoreAll > 0
+      ? Math.round((currentScoreAll / maxScoreAll) * 100)
+      : 0;
 
-        setTotalMaxScore(Number(data.totalMaxScore));
-      } catch (error) {
-        console.error("Error fetching total max score", error);
-      }
-    };
-    fetchTotalMaxScore();
-  }, [user, token]);
+  const percentEn =
+    maxScorePerLanguage > 0
+      ? Math.round((currentScoreEn / maxScorePerLanguage) * 100)
+      : 0;
 
-  const totalScores = userProgress.reduce(
-    (totals, progress) => {
-      return {
-        score_en: totals.score_en + (progress.score_en || 0),
-        score_fi: totals.score_fi + (progress.score_fi || 0),
-        score_ua: totals.score_ua + (progress.score_ua || 0),
-        score_ru: totals.score_ru + (progress.score_ru || 0),
-        maxScore: totals.maxScore + (progress.maxScore || 0),
-      };
-    },
-    { score_en: 0, score_fi: 0, score_ua: 0, score_ru: 0, maxScore: 0 }
-  );
+  const percentFi =
+    maxScorePerLanguage > 0
+      ? Math.round((currentScoreFi / maxScorePerLanguage) * 100)
+      : 0;
 
-  const totalScore =
-    (totalScores.score_en || 0) +
-    (totalScores.score_fi || 0) +
-    (totalScores.score_ua || 0) +
-    (totalScores.score_ru || 0);
+  const percentUa =
+    maxScorePerLanguage > 0
+      ? Math.round((currentScoreUa / maxScorePerLanguage) * 100)
+      : 0;
 
-  const totalMaxScoreAllLanguages = totalMaxScore * 4;
+  const percentRu =
+    maxScorePerLanguage > 0
+      ? Math.round((currentScoreRu / maxScorePerLanguage) * 100)
+      : 0;
+
   const toFraction = (score, max) => {
     if (max === 0) return 0;
     return score / max;
-  };
-  const toPercent = (score, max) => {
-    if (max === 0) return 0;
-    return Math.round((score / max) * 100);
   };
 
   return (
@@ -97,42 +89,42 @@ const ProgressScreen = ({ navigation }) => {
 
             <ProgressCard
               language={"Kokonaisedistyminen"}
-              percents={toPercent(totalScore, totalMaxScoreAllLanguages)}
-              totalMaxScoreAllLanguages={totalMaxScoreAllLanguages}
-              totalScore={totalScore}
-              value={toFraction(totalScore, totalMaxScoreAllLanguages)}
+              percents={percentAll}
+              totalMaxScoreAllLanguages={maxScoreAll}
+              totalScore={currentScoreAll}
+              value={toFraction(currentScoreAll, maxScoreAll)}
             />
 
             <ProgressCard
               language={"Englanniksi"}
-              percents={toPercent(totalScores.score_en, totalMaxScore)}
-              totalMaxScoreAllLanguages={totalMaxScore}
-              totalScore={totalScores.score_en}
-              value={toFraction(totalScores.score_en, totalMaxScore)}
+              percents={percentEn}
+              totalMaxScoreAllLanguages={maxScorePerLanguage}
+              totalScore={currentScoreEn}
+              value={toFraction(currentScoreEn, maxScorePerLanguage)}
             />
 
             <ProgressCard
               language={"Suomeksi"}
-              percents={toPercent(totalScores.score_fi, totalMaxScore)}
-              totalMaxScoreAllLanguages={totalMaxScore}
-              totalScore={totalScores.score_fi}
-              value={toFraction(totalScores.score_fi, totalMaxScore)}
+              percents={percentFi}
+              totalMaxScoreAllLanguages={maxScorePerLanguage}
+              totalScore={currentScoreFi}
+              value={toFraction(currentScoreFi, maxScorePerLanguage)}
             />
 
             <ProgressCard
               language={"Ukrainaksi"}
-              percents={toPercent(totalScores.score_ua, totalMaxScore)}
-              totalMaxScoreAllLanguages={totalMaxScore}
-              totalScore={totalScores.score_ua}
-              value={toFraction(totalScores.score_ua, totalMaxScore)}
+              percents={percentUa}
+              totalMaxScoreAllLanguages={maxScorePerLanguage}
+              totalScore={currentScoreUa}
+              value={toFraction(currentScoreUa, maxScorePerLanguage)}
             />
 
             <ProgressCard
               language={"Venäjäksi"}
-              percents={toPercent(totalScores.score_ru, totalMaxScore)}
-              totalMaxScoreAllLanguages={totalMaxScore}
-              totalScore={totalScores.score_ru}
-              value={toFraction(totalScores.score_ru, totalMaxScore)}
+              percents={percentRu}
+              totalMaxScoreAllLanguages={maxScorePerLanguage}
+              totalScore={currentScoreRu}
+              value={toFraction(currentScoreRu, maxScorePerLanguage)}
             />
           </View>
         </View>
